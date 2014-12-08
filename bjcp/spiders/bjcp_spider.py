235630f766393e3916f9c0c4b3d465cf75a18188
gpupu <jgpuado@gmail.com>
2014-12-08 17:30:23 +0100

    Accessing to the substyles web (not the best solution)

--- bjcp/spiders/bjcp_spider.py
+++ bjcp/spiders/bjcp_spider.py
@@ -1,35 +1,22 @@
+from scrapy.contrib.linkextractors.lxmlhtml import LxmlLinkExtractor
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.contrib.spiders import Rule, CrawlSpider
+from scrapy.selector import HtmlXPathSelector
 from bjcp.items import BeerTitleItem
 
 __author__ = 'Gabriel'
 
-import scrapy
-
-
-# class DmozSpider(scrapy.Spider):
-#     name = "dmoz"
-#     allowed_domains = ["dmoz.org"]
-#     start_urls = [
-#         "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
-#         "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
-#     ]
-#
-#     def parse(self, response):
-#         for sel in response.xpath('//ul/li'):
-#             item = DmozItem()
-#             item['title'] = sel.xpath('a/text()').extract()
-#             item['link'] = sel.xpath('a/@href').extract()
-#             item['desc'] = sel.xpath('text()').extract()
-#             yield item
-
-
-class BJCPSpider(scrapy.Spider):
+class BJCPSpider(CrawlSpider):
     name = "bjcp"
     allowed_domains = ["bjcp.org"]
     start_urls = [
         "http://www.bjcp.org/2008styles/catdex.php"
     ]
+    rules = (
+        Rule(SgmlLinkExtractor("style\d\d.php"), callback='parse_substyle', follow=False),
+    )
 
-    def parse(self, response):
+    def parse_start_url(self, response):
         for sel in response.xpath("//ul[@class='features']/li"):
             beer_item = BeerTitleItem()
             n = sel.xpath('a[@href]/text()').extract()
@@ -37,4 +24,17 @@ class BJCPSpider(scrapy.Spider):
                 beer_item['name'] = n[0]
                 beer_item['url'] = sel.xpath('a/@href').extract()[0]
                 yield beer_item
+
+    def parse_substyle(self, response):
+        resp = HtmlXPathSelector(response)
+        print "Respuesta del subestilo: "
+        yield resp
+        pass
+        #titles = hxs.select('//span[@class="pl"]')
+        items = []
+        # for titles in titles:
+        #     item = CraigslistSampleItem()
+        #     item ["title"] = titles.select("a/text()").extract()
+        #     item ["link"] = titles.select("a/@href").extract()
+        #     items.append(item)
 
